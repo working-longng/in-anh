fa0eaec28ac6d1edef271b494657b61e83ddc67d
long <longlop68@gmail.com>
2023-09-05 14:00:46 +0700

    w

--- In-Anh/In-Anh/RabitMQ/RabitMQConsumer.cs
+++ In-Anh/In-Anh/RabitMQ/RabitMQConsumer.cs
@@ -11,136 +11,88 @@ using ImageMagick;
 
 namespace In_Anh.RabitMQ
 {
-    public class RabitMQConsumer : IHostedService
-    {
-       
-            private IModel channel = null;
-            private IConnection connection = null;
+	public class RabitMQConsumer : IHostedService
+	{
 
-            // Initiate RabbitMQ and start listening to an input queue
-            private void Run()
-            {
-                // ! Fill in your data here !
-                var factory = new ConnectionFactory()
-                {
-                    HostName = "jinnie.shop",
-                    // port = 5672, default value
-                    VirtualHost = "/",
-                    UserName = "admin",
-                    Password = "admin"
-				};
+		private IModel channel = null;
+		private IConnection connection = null;
+		private void Run()
+		{
+			var factory = new ConnectionFactory()
+			{
+				HostName = "jinnie.shop",
+				VirtualHost = "/",
+				UserName = "admin",
+				Password = "admin"
+			};
+			connection = factory.CreateConnection();
+			channel = connection.CreateModel();
+			channel.QueueDeclare(queue: "image",
+								durable: true,
+								exclusive: false,
+								autoDelete: false,
+								arguments: null);
+			channel.BasicQos(prefetchSize: 0, prefetchCount: 5, global: false);
+			Console.WriteLine(" [*] Waiting for messages.");
+			var consumer = new EventingBasicConsumer(this.channel);
+			consumer.Received += OnMessageRecieved;
+			channel.BasicConsume(queue: "image",
+								autoAck: false,
+								consumer: consumer);
+		}
 
-                this.connection = factory.CreateConnection();
-                this.channel = this.connection.CreateModel();
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			Run();
+			return Task.CompletedTask;
+		}
 
-                // ! Declare an exchange, need to be updated !
-                //this.channel.ExchangeDeclare("exchange", "direct", true, false, null);
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			channel.Dispose();
+			connection.Dispose();
+			return Task.CompletedTask;
+		}
 
-                // A queue to read messages
-                this.channel.QueueDeclare(queue: "image",
-                                    durable: true,
-                                    exclusive: false,
-                                    autoDelete: false,
-                                    arguments: null);
-                //this.channel.QueueBind("queue.in", "exchange", "in");
-
-                //// A queue to write messages
-                //this.channel.QueueDeclare(queue: "queue.out",
-                //                    durable: true,
-                //                    exclusive: false,
-                //                    autoDelete: false,
-                //                    arguments: null);
-                //this.channel.QueueBind("queue.out", "exchange", "out");
-
-                this.channel.BasicQos(prefetchSize: 0, prefetchCount: 5, global: false);
-
-                Console.WriteLine(" [*] Waiting for messages.");
-
-                var consumer = new EventingBasicConsumer(this.channel);
-                consumer.Received += OnMessageRecieved;
-
-                this.channel.BasicConsume(queue: "image",
-                                    autoAck: false,
-                                    consumer: consumer);
-            }
-
-            public Task StartAsync(CancellationToken cancellationToken)
-            {
-                this.Run();
-                return Task.CompletedTask;
-            }
-
-            public Task StopAsync(CancellationToken cancellationToken)
-            {
-                this.channel.Dispose();
-                this.connection.Dispose();
-                return Task.CompletedTask;
-            }
-
-            // Publish a received  message with "reply:" prefix
-            private void OnMessageRecieved(object model, BasicDeliverEventArgs args)
-            {
-                var body = args.Body.ToArray();
-                //var message = Encoding.UTF8.GetString(body);
-                //Console.WriteLine(" [x] Received {0}", message);
-
-                //int dots = message.Split('.').Length - 1;
-
-                //// Publish a response
-                //string outMessage = "reply:" + message;
-                //body = Encoding.UTF8.GetBytes(outMessage);
-            var message = Encoding.UTF8.GetString(body);
-
-            var data = JsonConvert.DeserializeObject<RabitMQSendData>(message);
-
-
-            var fileName = data.FileName;
-            var filePath = data.Path +"\\"+ fileName;
-
-            using (var memoryStream = new MemoryStream(data.File))
-            {
-
-
-                string content = Encoding.UTF8.GetString(data.File);
-                if (Regex.IsMatch(content, @"<script|<cross\-domain\-policy",
-                    RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Multiline))
-                {
-
-                }
-
-                if (!Directory.Exists(data.Path))
-                {
-                    Directory.CreateDirectory(data.Path);
-                }
-
-
-                if (!File.Exists(filePath))
-                {
+		private void OnMessageRecieved(object model, BasicDeliverEventArgs args)
+		{
+			var body = args.Body.ToArray();
+			var message = Encoding.UTF8.GetString(body);
+			var data = JsonConvert.DeserializeObject<RabitMQSendData>(message);
+			var fileName = data.FileName;
+			var filePath = data.Path + "\\" + fileName;
+			using (var memoryStream = new MemoryStream(data.File))
+			{
+				string content = Encoding.UTF8.GetString(data.File);
+				if (Regex.IsMatch(content, @"<script|<cross\-domain\-policy",
+					RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Multiline))
+				{
+					return;
+				}
+				if (!Directory.Exists(data.Path))
+				{
+					Directory.CreateDirectory(data.Path);
+				}
+				if (!File.Exists(filePath))
+				{
 					using (FileStream f = File.Create(filePath))
 					{
-
 						f.Dispose();
 					}
 				}
-                memoryStream.Position = 0;
-                using (MagickImage image = new MagickImage(memoryStream))
-                {
-                    image.Format = MagickFormat.Jpeg;
-                    image.Quality = 100;
-                    image.Write(filePath);
-                    image.Dispose();
-                }
-                memoryStream.Close();
-            }
-            //this.channel.BasicPublish(exchange: "exchange",
-            //                     routingKey: "out",
-            //                     basicProperties: this.channel.CreateBasicProperties(),
-            //                     body: body);
-            //Console.WriteLine(" [x] Sent {0}", outMessage);
+				memoryStream.Position = 0;
+				using (MagickImage image = new MagickImage(memoryStream))
+				{
+					image.Format = MagickFormat.Jpeg;
+					image.Quality = 100;
+					image.Write(filePath);
+					image.Dispose();
+				}
+				memoryStream.Close();
+			}
+			Console.WriteLine(" [x] Done");
+			channel.BasicAck(deliveryTag: args.DeliveryTag, multiple: false);
+		}
 
-            Console.WriteLine(" [x] Done");
-                this.channel.BasicAck(deliveryTag: args.DeliveryTag, multiple: false);
-            }
-
-        }
+	}
 }
