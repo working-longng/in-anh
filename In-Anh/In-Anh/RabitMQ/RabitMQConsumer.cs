e76a48141bccbd2d5e7f33a0d1ec84a3e191ee3c
long <longlop68@gmail.com>
2023-09-13 23:47:28 +0700

    s

--- In-Anh/In-Anh/RabitMQ/RabitMQConsumer.cs
+++ In-Anh/In-Anh/RabitMQ/RabitMQConsumer.cs
@@ -19,7 +19,7 @@ namespace In_Anh.RabitMQ
 
         public IConfiguration _config;
 
-        public  IMongoCollection<OrderModel> _ordersCollection;
+        public IMongoCollection<OrderModel> _ordersCollection;
 
         public readonly IRabitMQProducer _rabitMQProducer;
 
@@ -27,7 +27,7 @@ namespace In_Anh.RabitMQ
         public RabitMQConsumer(IConfiguration config)
         {
             _config = config;
-           
+
 
         }
         private IModel channel = null;
@@ -115,67 +115,71 @@ namespace In_Anh.RabitMQ
 
         private async Task<bool> SaveImageAsync(string orderID, string urlImg, ImageType type)
         {
-            double price = 0;
+            try
+            {
+                double price = 0;
 
-            switch (type)
-            {
-                case ImageType.t4x6:
-                    price = 2000;
-                    break;
-                case ImageType.t6x9:
-                    price = 3000;
-                    break;
-                case ImageType.t9x12:
-                    price = 7000;
-                    break;
-                case ImageType.t10x15:
-                    price = 9000;
-                    break;
-                case ImageType.t13x18:
-                    price = 12000;
-                    break;
-                case ImageType.t15x21:
-                    price = 15000;
-                    break;
-                case ImageType.t20x30:
-                    price = 30000;
-                    break;
-                default:
-                    break;
-            }
-            var filter = Builders<OrderModel>.Filter.And(Builders<OrderModel>.Filter.Where(x => x.ListDetail.Any(y => y.OrderId == orderID)));
-            
-            var ip = Dns.GetHostEntry(Dns.GetHostName())
-   .AddressList
-   .First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-   .ToString();
-            var port = 0;
-            if (ip == "192.168.1.4")
-            {
-                port = 444;
-            }
-            else
-            {
-                port = 446;
-            }
-           var cdn =  _config["Cdn:UrlCdn"];
-
-            var client = new MongoClient(port == 444 ? _config["ImageMgDatabase:ConnectionString"] : _config["ImageMgDatabase:ConnectionString1"]);
-            var database = client.GetDatabase(_config["ImageMgDatabase:DatabaseName"]);
-            _ordersCollection = database.GetCollection<OrderModel>(_config["ImageMgDatabase:OrderCollectionName"]);
-            Thread.Sleep(1000);
-            var oldData = _ordersCollection.FindAsync(filter).Result.FirstOrDefault();
-            if (oldData != null)
-            {
-                var dataDetails = oldData.ListDetail.ToList();
-                var dataOrderDetail = dataDetails.Find(x => x.OrderId == orderID);
-                if (dataOrderDetail != null)
+                switch (type)
                 {
-                    var dataImg = dataOrderDetail.Images?.FirstOrDefault();
-                    if (dataImg == null)
+                    case ImageType.t4x6:
+                        price = 2000;
+                        break;
+                    case ImageType.t6x9:
+                        price = 3000;
+                        break;
+                    case ImageType.t9x12:
+                        price = 7000;
+                        break;
+                    case ImageType.t10x15:
+                        price = 9000;
+                        break;
+                    case ImageType.t13x18:
+                        price = 12000;
+                        break;
+                    case ImageType.t15x21:
+                        price = 15000;
+                        break;
+                    case ImageType.t20x30:
+                        price = 30000;
+                        break;
+                    default:
+                        break;
+                }
+                var filter = Builders<OrderModel>.Filter.And(Builders<OrderModel>.Filter.Where(x => x.ListDetail.Any(y => y.OrderId == orderID)));
+
+                var ip = Dns.GetHostEntry(Dns.GetHostName())
+       .AddressList
+       .First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+       .ToString();
+                var port = 0;
+                if (ip == "192.168.1.4")
+                {
+                    port = 444;
+                }
+                else
+                {
+                    port = 446;
+                }
+                var cdn = _config["Cdn:UrlCdn"];
+
+                var client = new MongoClient(port == 444 ? _config["ImageMgDatabase:ConnectionString"] : _config["ImageMgDatabase:ConnectionString1"]);
+
+                var database = client.GetDatabase(_config["ImageMgDatabase:DatabaseName"]);
+
+                _ordersCollection = database.GetCollection<OrderModel>(_config["ImageMgDatabase:OrderCollectionName"]);
+                Thread.Sleep(1000);
+                var oldData = _ordersCollection.FindAsync(filter).Result.FirstOrDefault();
+                if (oldData != null)
+                {
+                    var dataDetails = oldData.ListDetail.ToList();
+                    var dataOrderDetail = dataDetails.Find(x => x.OrderId == orderID);
+                    if (dataOrderDetail != null)
                     {
-                        
-                        dataOrderDetail.Images = new List<ImageModel>
+                        var dataImg = dataOrderDetail.Images?.FirstOrDefault();
+                        if (dataImg == null)
+                        {
+
+                            dataOrderDetail.Images = new List<ImageModel>
                         {
                             new ImageModel()
                             {
@@ -184,34 +188,47 @@ namespace In_Anh.RabitMQ
                                 OrginUrl= new List<string>{ cdn+":"+port+ "\\"+urlImg }
                             }
                         };
-                    }
-                    else
-                    {
-                        if (dataDetails.Find(x => x.OrderId == orderID)?.Images?.ToList().Find(x => x.Type == type) == null)
-                        {
-                            dataOrderDetail.Images.Add(
-                            new ImageModel()
-                            {
-                                Type = type,
-                                Price = price,
-                                OrginUrl = new List<string>{ cdn + ":" + port + "\\" + urlImg }
-                            }
-                        );
                         }
                         else
                         {
-                            dataDetails.Find(x => x.OrderId == orderID)?.Images?.ToList().Find(x => x.Type == type)?.OrginUrl.Add(cdn + ":" + port + "\\" + urlImg);
-                        }
-                        
-                    }
+                            if (dataDetails.Find(x => x.OrderId == orderID)?.Images?.ToList().Find(x => x.Type == type) == null)
+                            {
+                                dataOrderDetail?.Images?.Add(
+                                new ImageModel()
+                                {
+                                    Type = type,
+                                    Price = price,
+                                    OrginUrl = new List<string> { cdn + ":" + port + "\\" + urlImg }
+                                }
+                            );
+                            }
+                            else
+                            {
+                                dataDetails.Find(x => x.OrderId == orderID)?.Images?.ToList().Find(x => x.Type == type)?.OrginUrl.Add(cdn + ":" + port + "\\" + urlImg);
+                            }
 
-                    var update = Builders<OrderModel>.Update
-                     .Set(x => x.ListDetail, dataDetails);
-                    var data = await _ordersCollection.FindOneAndUpdateAsync(filter, update);
-                    return true;
+                        }
+
+                        var update = Builders<OrderModel>.Update
+                         .Set(x => x.ListDetail, dataDetails);
+                        var data = await _ordersCollection.FindOneAndUpdateAsync(filter, update);
+                        return true;
+                    }
                 }
+                return false;
             }
-            return false;
+            catch (Exception e)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(e.Message);
+
+
+                // flush every 20 seconds as you do it
+                File.AppendAllText("D:\\" + "log.txt", sb.ToString());
+                sb.Clear();
+                
+            }
         }
     }
 }
